28e15263ed880bd1571aaddee1aaff32e3459234
herbeon <lifeofamoof@gmail.com>
2026-08-08 14:53:05 -0400

    yay wings (still dunno how to make multiple work at once)

--- beetlegame/Player.cs
+++ beetlegame/Player.cs
@@ -3,8 +3,20 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	public const float Speed = 500.0f;
+	public const float JumpVelocity = -900.0f;
+	private AnimatedSprite2D _animatedSprite;
+	private AnimatedSprite2D _animatedwing1;
+	private AnimatedSprite2D _animatedwing2;
+
+
+	public override void _Ready()
+	{
+		_animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		_animatedwing1 = GetNode<AnimatedSprite2D>("AnimatedSprite2D2");
+		_animatedwing2 = GetNode<AnimatedSprite2D>("AnimatedSprite2D3");
+
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -17,21 +29,35 @@ public partial class Player : CharacterBody2D
 		}
 
 		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		if (Input.IsActionJustPressed("up") && IsOnFloor())
 		{
 			velocity.Y = JumpVelocity;
+			_animatedwing1.Play("default");
+			_animatedwing2.Play("default");
 		}
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector2 direction = Input.GetVector("left", "right", "up", "down");
 		if (direction != Vector2.Zero)
 		{
 			velocity.X = direction.X * Speed;
+			_animatedSprite.Play("default");
+			// if(direction.X > 0)
+			// {
+			// 	_animatedwing1.Play("default");
+			// }
+			// if(direction.X < 0)
+			// {
+			// 	_animatedwing2.Play("default");
+			// }
 		}
 		else
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			// _animatedSprite.Stop();
+			_animatedwing1.Stop();
+			_animatedwing2.Stop();
 		}
 
 		Velocity = velocity;
